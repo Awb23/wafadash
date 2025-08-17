7b2f8af8ee3c7e2457af52422f6aa3b11f241fdf
Awb23 <a.lahder3105@uca.ac.ma>
2025-08-17 17:25:45 +0000

    Deploy Django + React

--- wafadash/urls.py
+++ wafadash/urls.py
@@ -19,6 +19,11 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
+from django.contrib import admin
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('USERS.urls')),
