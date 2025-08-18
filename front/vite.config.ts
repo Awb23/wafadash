826a69c8c4541cd34913236096451308e7b9c0d3
Awb23 <a.lahder3105@uca.ac.ma>
2025-08-18 11:28:48 +0000

    FIX: Update Procfile to bind to Railway's PORT

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,12 +1,7 @@
-// front/vite.config.js
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-
-  // ✅ ADD THIS 'base' OPTION
-  // This tells Vite to build all asset links with a /static/ prefix
   base: '/static/',
 })
