82b7efbd579e80dbd9cfb0ea2e9868e434c7a3e8
Awb23 <a.lahder3105@uca.ac.ma>
2025-08-18 18:03:17 +0000

    FIX: Simplify ALLOWED_HOSTS setting to prevent crash

--- wafadash/settings.py
+++ wafadash/settings.py
@@ -174,6 +174,6 @@ TEMPLATES = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'front/dist/assets'),
+    os.path.join(BASE_DIR, 'front/dist'),
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
