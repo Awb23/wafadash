9d895f00534818ad6ad1ee62e5a03a9ca3219e85
Awb23 <a.lahder3105@uca.ac.ma>
2025-08-19 01:18:48 +0000

    Update ALLOWED_HOSTS

--- front/src/pages/userpa/api.js
+++ front/src/pages/userpa/api.js
@@ -1,15 +1,9 @@
-// front/src/api.js
 import axios from 'axios';
-
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
-
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
-    withCredentials: true, // IMPORTANT: This allows sending cookies
+    withCredentials: true,
 });
-
-// This automatically reads the 'csrftoken' cookie and puts it in the 'X-CSRFToken' header
 apiClient.defaults.xsrfCookieName = 'csrftoken';
 apiClient.defaults.xsrfHeaderName = 'X-CSRFToken';
-
 export default apiClient;
